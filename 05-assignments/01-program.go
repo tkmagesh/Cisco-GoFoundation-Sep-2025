74377a3c748148c32010e156c317b02c310ff0e9
Magesh Kuppan <tkmagesh77@gmail.com>
2025-09-22 15:28:33 +0530

    exercises

---
+++ 05-assignments/01-program.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+Build an interactive calculator
+
+	Display the menu
+	1. Add
+	2. Subtract
+	3. Multiply
+	4. Divide
+	5. Exit
+
+	if userchoice <= 4 and >= 1
+		Accept two operands
+		perform the corresponding operation
+		print the result
+		DISPLAY THE MENU again
+
+	if userchoice == 5
+		print "Thank you!"
+		exit the application
+
+	else
+		print "Invalid choice"
+		DISPLAY THE MENU again
+*/
+func main() {
+
+}
